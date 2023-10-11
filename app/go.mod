a57c6fb380e252f4bcfbffc4a88ee2b7635ff118
pengyeng <pengyeng@hotmail.com>
2023-10-11 16:47:01 +0100

    chang to use batch103

---
+++ app/go.mod
@@ -0,0 +1,9 @@
+module go-batch
+
+go 1.21
+
+require (
+	github.com/aws/aws-sdk-go v1.45.22 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pengyeng/batch103 v0.0.0-20231011153207-7b8ac0fe4da5 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -4,64 +4,6 @@ import (
 	"github.com/pengyeng/batch103"
 )
 
-<<<<<<< HEAD
-=======
-func DownloadFile(downloader *s3manager.Downloader, bucketName string, fileName string) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-
-	_, err = downloader.Download(
-		file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(fileName),
-		},
-	)
-
-	return err
-}
-
-type Processor struct{}
-
-func (p Processor) Process() error {
-	sess, err := session.NewSessionWithOptions(session.Options{
-		Profile: "default",
-		Config: aws.Config{
-			Region: aws.String(os.Getenv("REGION")),
-		},
-	})
-
-	if err != nil {
-		return err
-	}
-	fileName := os.Getenv("FILE_NAME")
-	bucketName := os.Getenv("BUCKET_NAME")
-	downloader := s3manager.NewDownloader(sess)
-	err = DownloadFile(downloader, bucketName, fileName)
-
-	if err != nil {
-		return err
-	}
-	file, _ := os.Open(fileName)
-	reader := csv.NewReader(file)
-	records, _ := reader.ReadAll()
-	fmt.Println("==== AWS Batch Start ====")
-	fmt.Println("Process File : ", fileName, " in ", bucketName)
-	for _, eachrecord := range records {
-		if eachrecord[1] == "TREASURE" {
-			fmt.Println(fileName, " : ", eachrecord[0])
-			time.Sleep(1 * time.Second)
-		}
-	}
-	fmt.Println("==== AWS Batch End ====")
-	return nil
-}
-
->>>>>>> 21db0e4 (minor update)
 func main() {
 
 	var myJobLauncher batch103.JobLauncher
